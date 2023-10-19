7ee1f4b30ca25920ebc6aa9f2c95f8c2fdd61de7
erik wesley <116817868+ErikWPN@users.noreply.github.com>
2023-10-19 11:27:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.scss'
 
-function titleComponet(){
+function titleComponent(){
     const elemH1 = document.createElement('h1')
     elemH1.innerHTML = 'Hello World 2'
     elemH1.classList.add('title')
